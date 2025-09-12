63ea17983e4a4a05c7b5ac3a848b853044bfad77
nanam173 <nnm.hori@gmail.com>
2025-09-12 13:52:05 +1000

    refactoring

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function Square({ value, onSquareClick}) {
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [ squares, setSquares ] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     if (squares[i]) {
